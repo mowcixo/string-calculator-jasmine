f1abf1344cec5ba482b33a2c6af214076fd5a985
Moisés Gramary Barbosa <moigramary@gmail.com>
2013-02-08 18:35:42 +0100

    Refactor of "si recibe una cadena vacía devolverá cero" test.

--- spec/StringCalculatorSpec.js
+++ spec/StringCalculatorSpec.js
@@ -1,8 +1,8 @@
 describe("StringCalculator -http://osherove.com/tdd-kata-1/-", function() {
-	var calc= StringCalculator;
+	var calc= StringCalculator
 
 	it("si recibe una cadena vacía devolverá cero", function() {
-		expect(calc.Add('')).toEqual(1);
-	});
+		expect(calc.Add('')).toEqual(0)
+	})
 	
-});
+})
--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,7 +1,7 @@
 var StringCalculator= {
 
 	Add: function(numbers) {
-		if( numbers == '' ) return 0;
+		if( numbers == '' ) return 0
 	}
 
-};
+}

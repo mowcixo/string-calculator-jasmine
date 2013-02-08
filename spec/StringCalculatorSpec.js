dcf898258400b6053175d5c43c2b84f93902c114
Moisés Gramary Barbosa <moigramary@gmail.com>
2013-02-08 18:33:56 +0100

    Red of "si recibe una cadena vacía devolverá cero" test.

---
+++ spec/StringCalculatorSpec.js
@@ -0,0 +1,8 @@
+describe("StringCalculator -http://osherove.com/tdd-kata-1/-", function() {
+	var calc= StringCalculator;
+
+	it("si recibe una cadena vacía devolverá cero", function() {
+		expect(calc.Add('')).toEqual(1);
+	});
+	
+});
---
+++ src/StringCalculator.js
@@ -0,0 +1,7 @@
+var StringCalculator= {
+
+	Add: function(numbers) {
+		if( numbers == '' ) return 0;
+	}
+
+};

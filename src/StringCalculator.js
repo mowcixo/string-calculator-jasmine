177be5954ea9685194d83e5416048c34cdae9fc0
Moisés Gramary Barbosa <moigramary@gmail.com>
2013-02-09 16:16:14 +0100

    Green of "si usamos como separador uno previamente establecido sigue sumando los números que reciba" test.

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -2,14 +2,22 @@ var StringCalculator= {
 
 	Add: function(numbers) {
 		if( !numbers ) numbers = '0'
+		var res= 0
 		
-		numbers = numbers.replace(/\n/g, ',');
-		
-		var res= 0,
-			splittedNumbers = numbers.split(',')
-		
-		for( var i= 0; i < splittedNumbers.length; i++ )
-			res += parseInt( splittedNumbers[ i ] )
+		if( numbers.indexOf('//') == 0 ) {
+			var separator= numbers.substring(2, 3),	
+				numbers= numbers.split('\n')[1],
+				splittedNumbers = numbers.split( separator )
+			
+			for( var i= 0; i < splittedNumbers.length; i++ )
+				res += parseInt( splittedNumbers[ i ] )
+		}else {
+			numbers = numbers.replace(/\n/g, ',');
+			var	splittedNumbers = numbers.split(',')
+			
+			for( var i= 0; i < splittedNumbers.length; i++ )
+				res += parseInt( splittedNumbers[ i ] )
+		}
 		
 		return res
 	}

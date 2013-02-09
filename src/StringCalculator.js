f4085e27ab209e827879bbd90d37c088402e4da2
Moisés Gramary Barbosa <moigramary@gmail.com>
2013-02-09 15:57:41 +0100

    Refactor of "si usamos como separador tanto salto de línea como comas sigue sumando los números que reciba" test.

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -3,13 +3,10 @@ var StringCalculator= {
 	Add: function(numbers) {
 		if( !numbers ) numbers = '0'
 		
-		var res= 0,
-			splittedNumbers= []
+		numbers = numbers.replace(/\n/g, ',');
 		
-		if( numbers.indexOf('\n') > -1 )
-			splittedNumbers= numbers.split('\n')
-		else
-			splittedNumbers= numbers.split(',')
+		var res= 0,
+			splittedNumbers = numbers.split(',')
 		
 		for( var i= 0; i < splittedNumbers.length; i++ )
 			res += parseInt( splittedNumbers[ i ] )

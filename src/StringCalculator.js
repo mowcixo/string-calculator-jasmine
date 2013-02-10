834c10d761cee849de44e3a7ff2ade10412901e2
Moisés Gramary Barbosa <moigramary@gmail.com>
2013-02-10 16:58:34 +0100

    Refactor of "si configuramos varios delimitadores" test.

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -5,23 +5,19 @@ var StringCalculator= {
 		var res = 0,
 			customSeparator = numbers.indexOf('//') == 0,
 			separator = customSeparator ? numbers.substring( 2, numbers.indexOf('\n') ) : ',',
-			multipleSeparator = numbers.indexOf('][') > 0,
+			multipleSeparator = ~numbers.indexOf(']['),
 			splittedNumbers = [],
 			negativeNumbers = []
 			
 		if( customSeparator ) {
+			numbers = numbers.split('\n')[1]
 			if( multipleSeparator ) {
-				var separators =
-					numbers
-					.substring( 2, numbers.indexOf('\n') )
-					.substring( 1 )
-				separators = separators.substring( 0, separators.length - 1 )
-				separators = separators.split('][')
-				var r = new RegExp('[' + separators.join('') + ']', 'g')		
-				numbers = numbers.split('\n')[1]
+				var separators = separator
+					.substring( 1, separator.length - 1 )
+					.split(']['),
+					r = new RegExp('[' + separators.join('') + ']', 'g')
 				numbers = numbers.replace( r, separator )
-			}else 
-				numbers = numbers.split('\n')[1]
+			}
 		}else
 			numbers = numbers.replace( /\n/g, ',' )
 			

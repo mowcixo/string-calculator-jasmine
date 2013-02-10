c77eb0a67e0aad3d2118591890c354873f2095ff
Moisés Gramary Barbosa <moigramary@gmail.com>
2013-02-10 15:17:13 +0100

    Refactor of "si el delimitador que configuremos tiene más de un dígito de longitud" test.

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -4,18 +4,18 @@ var StringCalculator= {
 		if( !numbers ) numbers = '0'
 		var res = 0,
 			customSeparator = numbers.indexOf('//') == 0,
-			separator = customSeparator ? numbers.substring(2, numbers.indexOf('\n')) : ',',
+			separator = customSeparator ? numbers.substring( 2, numbers.indexOf('\n') ) : ',',
 			splittedNumbers = [],
 			negativeNumbers = []
 			
 		if( customSeparator )
 			numbers = numbers.split('\n')[1]
 		else
-			numbers = numbers.replace(/\n/g, ',')
+			numbers = numbers.replace( /\n/g, ',' )
 			
 		splittedNumbers = numbers.split( separator )
 		
-		for( var i= 0; i < splittedNumbers.length; i++ ) {
+		for( var i= 0 ; i < splittedNumbers.length ; i++ ) {
 			var number = parseInt( splittedNumbers[ i ] )
 			if( number > 1000 ) continue
 			
